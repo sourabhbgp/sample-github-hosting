0254000c75ce0467c4cc436db65e54af2c155faa
sourabh kumar <sourabhbgp1994@gmail.com>
2020-10-20 00:59:38 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? "https://www.nptellogin.com" : "",
+  assetPrefix: "https://www.nptellogin.com",
 };
