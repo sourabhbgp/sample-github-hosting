8b805327c197b507424cba3503c5d51b9855cc0b
sourabh kumar <sourabhbgp1994@gmail.com>
2020-10-24 19:45:59 +0530

    analytics bug removed

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,16 @@ class MyDocument extends Document {
     return { ...initialProps };
   }
 
+  setGoogleTags = () => {
+    return {
+      __html: `
+       window.dataLayer = window.dataLayer || []; function gtag()
+      {dataLayer.push(arguments)}
+      gtag('js', new Date()); gtag('config', 'G-T6X021CDX2');
+      `,
+    };
+  };
+
   render() {
     return (
       <Html lang="en">
@@ -13,12 +23,8 @@ class MyDocument extends Document {
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-T6X021CDX2"
-          ></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'G-T6X021CDX2');
-          </script>
+          />
+          <script dangerouslySetInnerHTML={this.setGoogleTags()} />
         </Head>
         <body className="font-serif text-xl text-gray-800">
           <Main />
