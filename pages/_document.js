0e27c92b4ef302c362500ddc1f350930bfc87577
sourabh kumar <sourabhbgp1994@gmail.com>
2020-10-24 19:36:41 +0530

    google analytics added

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-T6X021CDX2"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-T6X021CDX2');
+          </script>
+        </Head>
         <body className="font-serif text-xl text-gray-800">
           <Main />
           <NextScript />
